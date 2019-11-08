cc57bf96bcc2843f749dc65b110ab00ad2b80a20
Jon Edwards <jkedwards@me.com>
2019-11-08 07:55:03 -0800

    add back in import to OkHttpProvider that autoformatting tried to reorder

--- android/src/main/java/com/kolohelios/reactnative/localapi/OkHttpProvider.java
+++ android/src/main/java/com/kolohelios/reactnative/localapi/OkHttpProvider.java
@@ -17,6 +17,7 @@ import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 
 public final class OkHttpProvider {
   public static OkHttpClient client;
