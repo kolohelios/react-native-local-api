66e31779589461a46fe942ea4768bc947ebb39ac
Jon Edwards <jkedwards@me.com>
2020-04-01 05:53:05 -0700

    fix undefined "response" issue in apiRequest and wrap function in a new Promise

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ import { NativeModules } from 'react-native';
 const { LocalApi } = NativeModules;
 
 export default {
-  apiRequest: async (url, method, params = {}, body = {}, setCookie = false) => {
-    const paramKeys = Object.keys(params);
+  apiRequest: async (url, method, params = {}, body = {}, setCookie = false) => new Promise((fulfilled, rejected) => {
+      const paramKeys = Object.keys(params);
 
-    if (paramKeys.length) {
-      url += '?';
-      paramKeys.forEach((key, index) => {
-        url += index > 0 ? '&' : '';
-        url += `${key}=${params[key]}`;
-      });
-    }
-    try {
-      const response = await LocalApi.apiRequest(url, method, body, setCookie);
-      const parsedResponse = JSON.parse(response);
-      return parsedResponse;
-    } catch (error) {
-      return response;
-    }
-  },
+      if (paramKeys.length) {
+        url += '?';
+        paramKeys.forEach((key, index) => {
+          url += index > 0 ? '&' : '';
+          url += `${key}=${params[key]}`;
+        });
+      }
+      try {
+        const response = await LocalApi.apiRequest(url, method, body, setCookie);
+        const parsedResponse = JSON.parse(response);
+        fulfilled(parsedResponse);
+      } catch (error) {
+        rejected(error);
+      }
+  }),
   clearCookies: LocalApi.clearCookies,
   pinCertificate: LocalApi.pinCertificate,
   setTimeout: LocalApi.setTimeout,
